cbed23e95353793d5c20d4c97046dc34086bcbac
Kazuki Mizunoya <kazukiftb3702@gmail.com>
2025-02-11 18:55:50 +0900

    最初の更新です

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
+    apiKey: "AIzaSyDmCi_b29dzGvaXuKkRNDMK3J2WnSWh4po",
+    authDomain: "school-index-10067.firebaseapp.com",
+    projectId: "school-index-10067",
+    storageBucket: "school-index-10067.firebasestorage.app",
+    messagingSenderId: "1004556224577",
+    appId: "1:1004556224577:web:4b5473ab25aa93e6bd1ec0",
+    measurementId: "G-FTKWPNS340"
+  };
 
 // Firebase を初期化
 firebase.initializeApp(firebaseConfig);
--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { initializeApp } from "firebase/app";
 
-  // Firestore からデータを取得
-  const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-  };
- 
-  const app = initializeApp(firebaseConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
